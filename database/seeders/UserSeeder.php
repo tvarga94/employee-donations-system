34e6a81d307d0010e2c9a458d990a712a00e9bb6
Varga Tamas <vargata1@gmail.com>
2025-05-03 23:04:08 +0200

    fixing userSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,13 +4,15 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::truncate();
+        DB::table('users')->delete();
+
         User::create([
             'name' => 'Test Employee',
             'email' => 'employee@example.com',
