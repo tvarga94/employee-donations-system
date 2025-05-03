1f385c647ba4b24014acaeb3f6f80ea295df6333
Varga Tamas <vargata1@gmail.com>
2025-05-03 22:55:32 +0200

    missing rules annotation added

--- app/Http/Requests/StoreDonationRequest.php
+++ app/Http/Requests/StoreDonationRequest.php
@@ -11,6 +11,9 @@ class StoreDonationRequest extends FormRequest
         return true;
     }
 
+    /**
+     * @return array<string, array<int, string>>
+     */
     public function rules(): array
     {
         return [
